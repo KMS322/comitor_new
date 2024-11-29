ce1f57ee72fc3aad4ae93fed1efe97bb48cdbb1c
KMS322 <kms930322@naver.com>
2024-11-29 11:46:39 +0900

    MAIN_URL change

--- routes/sns.js
+++ routes/sns.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const MAIN_URL = "http://localhost";
+const MAIN_URL = "http://comitor.shop";
 
 router.get("/kakao", passport.authenticate("kakao")); // 카카오톡 로그인 화면으로 redirect
 
